7895f0e669041b01e402d10d412adb9731929ac7
hereward99 <nicholas.manthorpe@gmail.com>
2026-09-09 08:36:13 -0300

    Update vite.config.ts
    fixed to chronicle-v2

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const base = mode === "production" ? "/chronicle/" : "/";
+  const base = mode === "production" ? "/chronicle-v2/" : "/";
 
   return {
     base,
